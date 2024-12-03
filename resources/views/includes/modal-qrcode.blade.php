bc98ee418d6681538e819d11b799186d275e8cf2
Taufik Ridho <taufikridho505@gmail.com>
2024-12-04 00:04:04 +0700

    fix: add icon download

--- resources/views/includes/modal-qrcode.blade.php
+++ resources/views/includes/modal-qrcode.blade.php
@@ -12,7 +12,7 @@
             </div>
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <a href="#" id="download-button" class="btn btn-primary">Download Gambar</a>
+                <a href="#" id="download-button" class="btn btn-primary"><i class="fas fa-download mr-1"></i>Download QR Code</a>
             </div>
         </div>
         <!-- /.modal-content -->
